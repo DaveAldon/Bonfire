4beeba061d1a0e28904199b978d53df43cf3bfff
DaveAldon <crawford@mail.gvsu.edu>
2017-08-12 01:39:49 -0400

    Updated text area visuals for editor and compiler. Fixed a few scrolling issues.

--- js/compiler.js
+++ js/compiler.js
@@ -2,6 +2,21 @@ document.getElementById("compilerStatus").innerHTML = "Compiler is <span id='sta
 document.getElementById("compilerStatus").innerHTML = "Compiler is <span id='statusOff'>Offline</span>";
 
 var compiler = ace.edit("cb");
-    compiler.setTheme("ace/theme/monokai");
-    compiler.getSession().setMode("ace/mode/javascript");
+compiler.setTheme("ace/theme/monokai");
+compiler.getSession().setMode("ace/mode/javascript");
 compiler.resize();
+compiler.getSession().setUseWrapMode(true);
+compiler.session.setValue("")
+
+compiler.setOptions({
+    //maxLines: 1,
+    readOnly: false,
+    autoScrollEditorIntoView: true,
+    highlightActiveLine: true,
+    printMargin: false,
+    showGutter: false,
+    scrollPastEnd: true,
+    autoScrollEditorIntoView: false,
+    mode: "ace/mode/javascript",
+    theme: "ace/theme/tomorrow_night_eighties"
+});
